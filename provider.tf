cd0a00adff79eee136b1759552a13c881f010a39
aldan <adanilkumar@outlook.com>
2024-06-15 08:37:53 +0100

    updated comments

--- provider.tf
+++ provider.tf
@@ -1,14 +1,15 @@
+# Define the AWS provider
 provider "aws" {
-  region     = "eu-west-2"
+  region = "eu-west-2"  # Specify the AWS region to use
 }
 
+# Configure the Terraform backend to use S3 for state storage and DynamoDB for state locking
 terraform {
   backend "s3" {
-    bucket = "aldan-s3-remote-state"
-    dynamodb_table = "state-lock"
-    key = "terraform/terraform.tfstate"
-    region = "eu-west-2"
-    encrypt = true
-    
+    bucket         = "aldan-s3-remote-state"  # Name of the S3 bucket
+    dynamodb_table = "state-lock"             # Name of the DynamoDB table for state locking
+    key            = "terraform/terraform.tfstate"  # Path within the bucket where the state file will be stored
+    region         = "eu-west-2"              # AWS region for the S3 bucket and DynamoDB table
+    encrypt        = true                     # Enable server-side encryption for the state file
   }
 }
--- remote-backend.tf
+++ remote-backend.tf
@@ -1,34 +1,34 @@
-#s3 config 
+# Define an S3 bucket to be used for storing Terraform remote state
 resource "aws_s3_bucket" "remote-backend-bucket" {
-    bucket = "aldan-s3-remote-state"
-    }
+    bucket = "aldan-s3-remote-state" #make sure the bucket name is unique 
+}
 
+# Enable versioning on the S3 bucket
 resource "aws_s3_bucket_versioning" "backend-bucket-versioning" {
     bucket = aws_s3_bucket.remote-backend-bucket.id
     versioning_configuration {
-      status = "Enabled"
+        status = "Enabled"  # Enable versioning to keep multiple versions of an object
     }
 }
 
+# Enable server-side encryption on the S3 bucket
 resource "aws_s3_bucket_server_side_encryption_configuration" "backend-encryption" {
     bucket = aws_s3_bucket.remote-backend-bucket.id
     rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+        apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"  # Using AES256 encryption for objects stored in the bucket
+        }
     }
-  
 }
 
-#DYNAMODB
-
+# Create a DynamoDB table for Terraform state locking
 resource "aws_dynamodb_table" "backend-statelock" {
-  name             = "state-lock"
-  billing_mode     = "PAY_PER_REQUEST"
-  hash_key         = "LockID"
+    name             = "state-lock"
+    billing_mode     = "PAY_PER_REQUEST"  # Use on-demand billing for DynamoDB
+    hash_key         = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+    attribute {
+        name = "LockID"  # Define the primary key for the table
+        type = "S"       # Specify the type of the primary key (String)
+    }
 }
